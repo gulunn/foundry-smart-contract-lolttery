6814673b41672a69c28ec66effe272d7ff3c3182
LiuZichen <zichen.L@outlook.com>
2024-07-22 14:37:24 +0800

    innitial commit

---
+++ src/Raffle.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {VRFCoordinatorV2} from "@chainlink/contracts/src/v0.8/VRFCoordinatorV2.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
+contract Raffle is VRFConsumerBaseV2 {
+    /* Errors */
+    error Raffle__NotEnoughEth();
+    error Raffle_NotOpen();
+
+    /* State variables */
+    // Chainlink VRF Variables
+    uint256 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Lottery Variables
+    uint256 private immutable i_interval;
+    uint256 private immutable i_entranceFee;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    address payable[] private s_players;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    /* Functions */
+    constructor(
+        uint256 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit,
+        address vrfCoordinatorV2
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_subscriptionId = subscriptionId;
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_entranceFee = entranceFee;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEth();
+        }
+        s_players.push(payable(msg.sender));
+    }
+
+    function requestRandomWords() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+}
